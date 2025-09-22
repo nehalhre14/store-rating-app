2b1201d1dd470e0ef999f5d9ff0c6c724ac59462
nehalhre14 <145849296+nehalhre14@users.noreply.github.com>
2025-09-22 15:58:24 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Store Rating App</h1>
+      <p>Frontend is working 🚀</p>
+    </div>
+  );
+}
+
+export default App;
